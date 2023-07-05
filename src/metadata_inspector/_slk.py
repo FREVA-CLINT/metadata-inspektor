16d578f6f683bab2fcf352c1685f1faed4464bef
antarcticrainforest <martin.bergemann@posteo.org>
2023-07-05 19:57:47 +0200

    Use a module load command instead of fixed binary paths.

--- src/metadata_inspector/_slk.py
+++ src/metadata_inspector/_slk.py
@@ -12,29 +12,51 @@ from getpass import getuser
 import json
 from pathlib import Path
 import os
+import re
+import shutil
 import warnings
-from subprocess import run, PIPE, SubprocessError
+from subprocess import run, PIPE, SubprocessError, CalledProcessError
 
 import requests
 
-SLK_PATH = "/sw/spack-levante/slk-3.3.21-5xnsgp/bin"
-JDK_PATH = "/sw/spack-levante/openjdk-17.0.0_35-k5o6dr/bin"
-JAVA_HOME = "/sw/spack-levante/openjdk-17.0.0_35-k5o6dr"
+MODULES_CMD_ENV = "MODULES_CMD"
+SLK = "slk"
 
 SESSION_PATH = Path("~").expanduser() / ".slk" / "config.json"
 
 
-def get_env() -> dict[str, str]:
-    """Prepare the environment variables for slk."""
+def load_module() -> dict[str, str]:
+    """Load the slk module."""
+    module_path = os.environ.get(
+        MODULES_CMD_ENV, "/usr/share/Modules/libexec/modulecmd.tcl"
+    )
+    if shutil.which("slk") is not None:
+        return os.environ.copy()
+    _env: dict[str, str] = {}
+    try:
+        res = (
+            run(
+                [module_path, "python", "load", SLK],
+                check=True,
+                stdout=PIPE,
+                stderr=PIPE,
+            )
+            .stdout.decode()
+            .split("\n")
+        )
+    except (FileNotFoundError, CalledProcessError) as error:
+        warnings.warn(f"Could not load {SLK}: {error}")
+        return _env
+    for line in res:
+        if "=" in line:
+            key, _, value = line.partition("=")
+            new_key = (re.findall(r"\['([^']*)", key.strip()) or [""])[0]
+            if new_key.strip():
+                _env[new_key.strip()] = value.strip().replace("'", "")
+    return _env
 
-    env = os.environ.copy()
-    env["PATH"] = f"{SLK_PATH}:{env['PATH']}"
-    env["PATH"] = f"{JDK_PATH}:{env['PATH']}"
-    env["JAVA_HOME"] = "{JAVA_HOME}"
-    return env
 
-
-def get_slk_metadata(input_path: str) -> str:
+def get_slk_metadata(input_path: str) -> dict[str, dict[str, str]]:
     """Extract dataset metdata from path in the hsm.
 
     Parameters
@@ -49,13 +71,14 @@ def get_slk_metadata(input_path: str) -> str:
     """
     command = ["slk_helpers", "metadata", input_path]
     try:
-        res = run(command, env=get_env(), check=True, stdout=PIPE, stderr=PIPE)
+        res = run(
+            command, env=load_module(), check=True, stdout=PIPE, stderr=PIPE
+        )
     except SubprocessError as error:  # pragma: no cover
         warnings.warn(
-            f"Error: could not get metdata: {error}"
+            f"Error: could not get meta-data: {error}"
         )  # pragma: no cover
-        return ""  # pragma: no cover
-    lines: list[str] = []
+        return {}  # pragma: no cover
     # This needs to be done because the output of the command is only nearly
     # yaml. That is the ":" for the first keys are missing:
     # For example:
@@ -66,10 +89,19 @@ def get_slk_metadata(input_path: str) -> str:
     #     var_name: tas
     # Since yaml needs could not handle this we have to add the ':' to the
     # keys manually.
-    for line in [o.strip() for o in res.stdout.decode().split("\n")]:
-        if line.strip().lower().startswith("keywords:"):
-            lines.append(line)
-    return "\n".join(lines)
+    data: dict[str, dict[str, str]] = {}
+    for line in res.stdout.decode().split("\n"):
+        if line.startswith("netcdf") or line.startswith("document"):
+            main_key = line.strip()
+            data[main_key] = {}
+            continue
+        if line and not line[0].strip():
+            key, _, value = line.partition(":")
+            current_key = key.strip()
+            data[main_key][current_key] = value.strip()
+        elif line:
+            data[main_key][current_key] += line.strip()
+    return data
 
 
 def get_expiration_date() -> datetime:
@@ -128,4 +160,4 @@ def login() -> None:
         _login_via_request(passwd)
     elif diff <= 0:
         print("Your session has expired, login to slk")
-        run(["slk", "login"], shell=False, check=True, env=get_env())
+        run(["slk", "login"], shell=False, check=True, env=load_module())
